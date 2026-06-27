fc3b37cd05eda2dc8664a4fff459c4041b7fc02a
asharfSahajanand <ashraf.sahajanand@gmail.com>
2026-06-27 18:38:21 +0530

    first commit

--- reward.js
+++ reward.js
@@ -22,7 +22,7 @@
   var UTM_HIT_COUNT_KEY    = 'utmRewardHitCount';
 
   // -- Hardcoded reward ad unit -------------------------------------
-  var REWARD_AD_UNIT = '/23353868385/gamtreasure.com_reward';
+  var REWARD_AD_UNIT = '/23353868385/leveluphup.com_reward';
 
   // -- Debug helper ----------------------------------------------
   function dbg(msg, data, color) {
